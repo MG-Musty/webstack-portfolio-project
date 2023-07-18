277b1d402d0e73698ae7bb49dbd81254727b8ae4
MG-Musty <mustaphaaliyugaladima@gmail.com>
2023-07-18 13:30:29 +0100

    Added files and changes required for deployment

--- webstack-specialization/crm_main/settings.py
+++ webstack-specialization/crm_main/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-import dj_database_url
+#import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,8 +121,8 @@ DATABASES = {
 
 #DATABASE_URL.
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
