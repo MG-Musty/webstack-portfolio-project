ed8602d4872adad617b7ae47cbf95eccbbb3230a
MG-Musty <mustaphaaliyugaladima@gmail.com>
2023-07-22 14:16:15 +0100

    Added files and changes required for deployment

--- webstack-specialization/crm_main/settings.py
+++ webstack-specialization/crm_main/settings.py
@@ -29,8 +29,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-
-#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!*ndp-(t$)0cgpl!q4bh^t^o%&*z8!=&g-*e+bocov1h#5zsux')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 
